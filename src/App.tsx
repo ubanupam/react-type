7f4333689ad660f5bb219c046709ff8158fcedb7
ubanupam <ubanupam@gmail.com>
2023-05-10 20:48:56 +0530

    import path has been modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,44 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React,{ Suspense, useEffect, useState } from 'react';
 import  axios  from "axios";
 import './App.css';
-import Loading from './components/Loading';
 // import Table from './components/table/Table';
+import Loading from './components/Loading';
+import Pagination from './components/pagination/Pagination';
 
-const Table = React.lazy(()=>import("./components/table/Table"))
+const Table = React.lazy(()=> import('./components/table/Table'));
 
 function App() {
   const [data, setData] = useState<string[]>([])
-  // const [loading, setLoading] = useState<boolean>(false)
+  const [pageNo, setPageNo] = useState<number>(1)
+  const [itemsPerPages, setitemsPerPages] = useState<number>(10)
+  const [loading, setLoading] = useState<boolean>(false)
   useEffect(() => {
     const url = "https://jsonplaceholder.typicode.com/posts";
-    // setLoading(true);
+    setLoading(true);
     axios.get(url)
       .then((res:any) => {
         setData(res.data);
-        // setLoading(false);
+        setLoading(false);
       });
   }, []);
   return (
     <div className="aie arm">
       <div className='fx gg add bul cst'>
+      {
+        loading ? <Loading />: 
+        <>
         <Suspense fallback={<Loading/>}>
-            <Table data={data} />
+        <Table data={data} pageNo={pageNo} itemsPerPages={itemsPerPages} />
+        <Pagination 
+          data={data}
+          pageNo={pageNo}
+          setPageNo={setPageNo}
+          itemsPerPages={itemsPerPages}
+          setitemsPerPages={setitemsPerPages}
+        />
         </Suspense>
+        </>
+        }
       </div>
     </div>
   );
